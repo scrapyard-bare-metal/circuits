c41b4a82fab7bbb9575562ebc12bb754683a258d
Angel Gonzalez <usftechacg@gmail.com>
2026-07-07 22:28:36 -0400

    (0.5.0) - Init Commit

---
+++ IntegratedCircuit.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BareMetal\Circuits;
+
+use BareMetal\Contracts\Circuits\IntegratedCircuit as IntegratedCircuitContract;
+
+class IntegratedCircuit implements IntegratedCircuitContract
+{
+
+}
